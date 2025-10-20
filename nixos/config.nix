e578b42d3ed50a6322d926fbb9ef3d99ac163bbb
winona <winnie@winneon.moe>
2025-10-20 01:11:46 -0400

    remove firmware files

--- nixos/config.nix
+++ nixos/config.nix
@@ -26,7 +26,7 @@ in {
     firmware = [
       (pkgs.stdenvNoCC.mkDerivation (final: {
         name = "brcm-firmware";
-        src = ../firmware/brcm;
+        src = ~/firmware/brcm;
         installPhase = ''
           mkdir -p $out/lib/firmware/brcm
           cp ${final.src}/* "$out/lib/firmware/brcm"
