03f795a6ed715378727643a76ac5d0d595b23f19
winona <winnie@winneon.moe>
2025-11-04 00:58:31 -0500

    additional pipewire config

--- nixos/pipewire_sink.nix
+++ nixos/pipewire_sink.nix
@@ -10,6 +10,7 @@ let
     rev = "971833705c746b1b02e5a844c4ca7e2d86582dc9";
     sha256 = "iPS2wVNdH02NpDsymtMIqWlDMPXLdIBTdbH4uYe9OIE=";
   };
+  
   originalTweeterFilePath = "/usr/share/pipewire/devices/apple/macbook_pro_t2_16_1_tweeters-48k_4.wav";
   newTweeterFilePath = "${t2AppleAudioDSP}/firs/macbook_pro_t2_16_1_tweeters-48k_4.wav";
   originalWooferFilePath = "/usr/share/pipewire/devices/apple/macbook_pro_t2_16_1_woofers-48k_4.wav";
@@ -26,6 +27,6 @@ let
 in
 {
   services.pipewire.configPackages = [
-    (pkgs.writeTextDir "share/pipewire/pipewire.conf.avail/10-t2_161-sink.conf" (builtins.readFile "${dspSinkConfig}"))
+    (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-t2_161-sink.conf" (builtins.readFile "${dspSinkConfig}"))
   ];
 }
--- shared_config.nix
+++ shared_config.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    curl brave fastfetch git neovim qbittorrent wget vuetorrent
+    curl brave fastfetch git neovim python3Minimal qbittorrent wget vuetorrent
   ];
 }
